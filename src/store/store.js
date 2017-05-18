5cfc4f851a1c6c02542ca90fe46614c2e04a6882
destillat89 <destillat@bk.ru>
2017-05-18 21:47:13 +0300

    v3

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,22 @@
-import configureStore from './configureStore';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers/index';
+//import { ping } from './enhancers/ping'
+// import { createLogger } from 'redux-logger'
+import thunk from 'redux-thunk';
+
+function configureStore(initialState) {
+  //const logger = createLogger()
+  const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
+
+  // if (module.hot) {
+  //   module.hot.accept('../reducers', () => {
+  //     const nextRootReducer = require('../reducers')
+  //     store.replaceReducer(nextRootReducer)
+  //   })
+  // }
+
+  return store;
+}
 
 
 export const store=configureStore();
